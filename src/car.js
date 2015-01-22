a5fc2e6ab771d48e35cb1e598ed36697de6ec584
Abraham Hall <abrahamhall84@gmail.com>
2015-01-21 20:08:25 -0800

    finished

--- src/car.js
+++ src/car.js
@@ -1,13 +1,53 @@
 function Car(make, model, year, color){
-
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.state = "off";
+	this.previous_owners = [];
+	this.current_owner = "Manufacturer";
+	this.passengers = [];
 }
 
 Car.prototype.sale = function(newOwner){
-
+	this.previous_owners.push(this.current_owner);
+	this.current_owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
-
+	this.color = newColor;
 };
 
+Car.prototype.start = function() {
+	this.state = "on";
+};
+Car.prototype.off = function() {
+	this.state = "off";
+};
+Car.prototype.driveTo = function(destination) {
+	if (this.state === "on") {
+		console.log("driving to " + destination);
+	}
+};
+Car.prototype.park = function() {
+	if (this.state === "off") {
+		console.log("parked!!");
+	}
+};
+
+Car.prototype.pick_up = function(name) {
+	if (this.state === "on") {
+		this.passengers.push(name);
+		console.log("driving to pick up " + name);	
+	}
+};
+Car.prototype.dropOff = function(name) {
+	if(this.state === "on"){
+		this.passengers.indexOf(this === -1);
+		this.passengers.pop(name);
+	}	
+};
+
+
 module.exports=Car;
+
